b545eb7da1b04f599d0bd36fb0799b6ecf12d183
kvojps <joseferreira.santosjunior@upe.br>
2022-04-20 10:33:08 -0300

    adicionando logo a navbar

--- src/componentes/layout/Navbar.js
+++ src/componentes/layout/Navbar.js
@@ -3,11 +3,17 @@ import Container from './Container'
 
 import styles from './assets/Navbar.module.css'
 
+import logo from '../../img/logo.png'
+
 function Navbar() {
     return(
         <nav className={styles.navbar}>
             <Container>
-                <Link to='/' className={styles.slogan}><p>Controle seu peso</p></Link>
+                <Link to='/' className={styles.slogan}>
+                    <p>Controle seu peso</p>
+                    <img src={logo} alt="logo" className={styles.logo} />    
+                </Link>
+                
                 <ul className={styles.list}>
                    <li className={styles.item}><Link to="/dashboard">DashBoard</Link></li> 
                    <li className={styles.item}><Link to="/atualizar-dados">Atualizar Dados</Link></li>
