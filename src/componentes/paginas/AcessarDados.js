0b9df8ba34275e3b81abca1575d0846ab03356b1
kvojps <joseferreira.santosjunior@upe.br>
2022-04-21 20:51:30 -0300

    adicionando elementos da pagina de acessar dados

--- src/componentes/paginas/AcessarDados.js
+++ src/componentes/paginas/AcessarDados.js
@@ -1,8 +1,30 @@
+import styles from '../formulario/assets/Campos.module.css'
+import stylesAcessarDados from './assets/AcessarDados.module.css'
+import {Link} from 'react-router-dom'
+import logoCadastro from '../../img/logo-cadastro.png'
+
 function AcessarDados() {
     return (
-        <div>
-            <h1>Acessar dados</h1>
-        </div>
+        <section className={stylesAcessarDados.main}>
+            <div className={styles.container_home}>
+                <div>
+                    <img src={logoCadastro} alt="" className={styles.img}/>
+                </div>
+                <div className={styles.formulario}>
+                    <h1>Acessar meus dados</h1>
+                    <p>Qual o seu e-mail</p>
+                    <div className={styles.max_width}>
+                        <input type="email" placeholder="Email" className={styles.input_dados}/>
+                    </div>
+                    <div>
+                        <input type="button" value="Acessar" id="" className={styles.botao_finalizar}/>
+                        <Link to="/" className={styles.link}>
+                            Não inseriu seus dados? Clique aqui!
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
