e4d3be594bb1f460e0e02cbb8727c116ad92516a
Junior <josebazjunior09@gmail.com>
2022-04-21 11:37:51 -0300

    adicionando conteudo do componente comparativo na dashboard

--- src/componentes/dashboard/Comparativo.js
+++ src/componentes/dashboard/Comparativo.js
@@ -4,6 +4,28 @@ function Comparativo() {
     return (
         <section className={styles.main}>
             <h1>Comparativo</h1>
+            <div className={styles.container_comparativo}>
+                <div className={styles.bloco_1}>
+                    <h1>-0.5kg</h1>
+                    <p>última medida</p>
+                    <p>(55.0kg)</p>
+                </div>
+                <div className={styles.bloco_2}>
+                    <h1>-0.5kg</h1>
+                    <p>última medida</p>
+                    <p>(55.0kg)</p>
+                </div>
+                <div className={styles.bloco_3}>
+                    <h1>-0.5kg</h1>
+                    <p>última medida</p>
+                    <p>(55.0kg)</p>
+                </div>
+                <div className={styles.bloco_4}>
+                    <h1>-0.5kg</h1>
+                    <p>última medida</p>
+                    <p>(55.0kg)</p>
+                </div>
+            </div>
         </section>
     )
 }
