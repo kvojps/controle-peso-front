046e0d7b016dfd644c79bc35a3b7bb723917abb6
kvojps <joseferreira.santosjunior@upe.br>
2022-04-27 10:24:37 -0300

    refatorando app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './componentes/paginas/Home'
 import DashBoard from './componentes/paginas/Dashboard';
@@ -15,15 +15,15 @@ function App() {
     <div>
 
       <Router>
-        <Navbar/>
-        
+        <Navbar />
+
         <Routes>
-          <Route exact path = "/" element={<Home/>}></Route>
-          <Route path = "/dashboard" element={<DashBoard/>}></Route>
-          <Route path = "/atualizar-dados" element={<AtualizarDados/>}></Route>
-          <Route path = "/contato" element={<Contato/>}></Route>
-          <Route path = "/sobre" element={<Sobre/>}></Route>
-          <Route path = "/acessar-dados" element={<AcessarDados/>}></Route>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/dashboard" element={<DashBoard />}></Route>
+          <Route path="/atualizar-dados" element={<AtualizarDados />}></Route>
+          <Route path="/contato" element={<Contato />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
+          <Route path="/acessar-dados" element={<AcessarDados />}></Route>
         </Routes>
 
       </Router>
